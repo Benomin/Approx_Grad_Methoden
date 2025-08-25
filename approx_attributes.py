272fd7c878dd2bedb906ba5e8f56d2876f0267cf
benomin <benjamin.weis@uni-bielefeld.de>
2025-08-25 11:38:23 +0200

    Resnet änderung und grafiken

--- approx_attributes.py
+++ approx_attributes.py
@@ -3,15 +3,18 @@ import numpy as np
 class ApproxAttribution():
     def __init__(self,model):
         self.model = model
+        self.device = next(self.model.parameters()).device
 
     def grad_approx(self,h,X,target=0):
         print("Grad Approx")
         ret = []
         for elem in X:
             for index, value in np.ndenumerate(elem):
+                elem = elem.to(self.device)
                 new = elem.clone()
                 new[index] += h
-                res = np.abs(((self.model(new) - self.model(elem))/h).detach().numpy())
+                res = np.abs(((self.model(new) - self.model(elem))/h).detach().cpu().numpy())
+                #test
                 ret.append(res)
                 #print(res)
         return np.array(ret)[:,0,target]
@@ -20,17 +23,18 @@ class ApproxAttribution():
         ret = []
         for elem in X:
             for index, value in np.ndenumerate(elem):
+                elem = elem.to(self.device)
                 new = elem.clone()
                 new[index] += h
                 attr = 0
                 for i in range(1,riemann_step+1):
                     scaled_new  = (baseline + i/riemann_step) * (new - baseline)
                     scaled_elem = (baseline + i/riemann_step) * (elem - baseline)
-                    attr+= ((self.model(scaled_new) - self.model(scaled_elem))/h).detach().numpy()
+                    attr+= ((self.model(scaled_new) - self.model(scaled_elem))/h).detach().cpu().numpy()
 
 
                 val = (attr/riemann_step) * (value-baseline)
-                if np.array([np.isnan(j) for j in self.model(scaled_new).detach().numpy()]).any():
+                if np.array([np.isnan(j) for j in self.model(scaled_new).detach().cpu().numpy()]).any():
                     print("Error")
                     print((self.model(scaled_new)))
 
@@ -44,12 +48,13 @@ class ApproxAttribution():
         ret = []
         for elem in X:
             for index, value in np.ndenumerate(elem):
+                elem = elem.to(self.device)
                 new = elem.clone()
                 new[index] += h
 
 
 
-                ret.append(-1 * value * ((self.model(new) - self.model(elem))/h).detach().numpy())
+                ret.append(-1 * value * ((self.model(new) - self.model(elem))/h).detach().cpu().numpy())
         return np.array(ret)[:,0,target]
 
 
